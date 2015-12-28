5100dda1b4058405eebe4c2aa60bb6d7b9a178c7
Tomoaki Kawada <i@yvt.jp>
2015-12-28 15:37:03 +0900

    added publish script

---
+++ build-gh-pages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "[ Hit ENTER to continue, Ctrl+C to abort. ]"
+read
+
+on_error() {
+	echo "Build failed."
+	exit 1
+}
+
+echo "-- Creating the git branch gh-pages."
+git branch -f gh-pages || on_error
+
+echo "-- git checkout gh-pages"
+git checkout gh-pages || on_error
+
+echo "-- Building."
+npm run-script build || on_error
+
+echo "-- Adding built files."
+git add -f examples/js/bundle.js || on_error
+
+echo "-- Commiting."
+git commit -m "Automated build by build-gh-pages.sh"
+
+echo "[ Push to origin? Hit ENTER to continue, Ctrl+C to abort. ]"
+read
+
+echo "-- Publishing."
+git push origin gh-pages --force || on_error
+
+echo "-- Done."
+
