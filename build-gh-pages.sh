96433b97e1d9e5e10c8b0c0f44320d08eed4ec13
Tomoaki Kawada <i@yvt.jp>
2016-01-01 16:27:17 +0900

    use symlinks again

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -20,6 +20,13 @@ npm run-script build || on_error
 echo "-- Adding built files."
 git add -f example-assets/js/bundle.js || on_error
 
+for f in _includes _layouts assets; do
+	echo "-- Resolving symlink.: $f"
+	git rm $f || on_error
+	cp -R _root/$f $f || on_error
+	git add $f || on_error
+done
+
 echo "-- Commiting."
 git commit -m "Automated build by build-gh-pages.sh"
 
